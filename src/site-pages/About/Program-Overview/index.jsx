b25209fbd9af1131f0a12cd4dee7134cbd03e0d2
nate2427 <nlbaker@umich.edu>
2020-10-12 23:01:08 -0500

    finished out the program overview page

--- src/site-pages/About/Program-Overview/index.jsx
+++ src/site-pages/About/Program-Overview/index.jsx
@@ -1,20 +1,24 @@
-import { Grid } from "@material-ui/core";
-import clsx from "clsx";
+import { Grid, Typography } from "@material-ui/core";
+// import clsx from "clsx";
 import React from "react";
+import Title from "../../../shared/TitleComponent";
+import ReactCardFlip from "react-card-flip";
 
 import { useStyles } from "./styles";
 import { get_content } from "../../../shared/Http";
 import Spinner from "../../../assets/Spinner@2x.png";
+import clsx from "clsx";
 
 export default function ProgramIntroduction() {
   const classes = useStyles();
-  const [content, setContent] = React.useState({});
+  const [content, setContent] = React.useState(null);
 
   // get the content from the CMS
   React.useEffect(() => {
-    const CMS_ENDPOINT = "umsep-program-introduction";
+    const CMS_ENDPOINT = "umsep-program-overview";
     get_content(CMS_ENDPOINT).then((data) => {
-      setContent(data);
+      setContent(data.data);
+      console.log(data.data);
     });
   }, []);
 
@@ -22,7 +26,108 @@ export default function ProgramIntroduction() {
 
   return (
     <Grid className={classes.container}>
-      <Grid container className={classes.innerContainer}></Grid>
+      <Grid container className={classes.innerContainer}>
+        {/* hero container */}
+        <Grid container className={classes.heroContainer}>
+          <Title
+            title={content ? content.title_subtitle.title : ""}
+            subtitle={content ? content.title_subtitle.subtitle : ""}
+          />
+        </Grid>
+        {/* paragraphs container */}
+        <Grid
+          container
+          justify="center"
+          className={classes.paragraphsContainer}
+        >
+          <Grid container lg={10} xl={8}>
+            <Typography variant="body1" className={classes.smallText}>
+              {content && content.paragraph1}
+            </Typography>
+            <Typography variant="h1" className={classes.largeText}>
+              {content && content.bigText}
+            </Typography>
+            <Typography variant="body1" className={classes.smallText}>
+              {content && content.paragraph2}
+            </Typography>
+            <Typography variant="body1" className={classes.smallText}>
+              {content && content.paragraph3}
+            </Typography>
+          </Grid>
+        </Grid>
+        {/* statistical overview */}
+        <Grid container justify="flex-start" className={classes.statOverview}>
+          <Title
+            title={content ? content.stat_title_subtitle.title : ""}
+            subtitle={content ? content.stat_title_subtitle.subtitle : ""}
+            sidebarColor={"blueBorder"}
+            fontColor={"fontColorBlue"}
+          />
+          <Grid
+            container
+            className={classes.tileOuterContainer}
+            justify="center"
+          >
+            <Grid
+              container
+              justify="center"
+              className={classes.tileContainer}
+              item
+              xs={11}
+              sm={10}
+              xl={7}
+            >
+              {content &&
+                content.quick_stats.map(({ front, back }, key) => {
+                  return <Tile front={front} back={back} key={key} />;
+                })}
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
+
+const Tile = ({ front, back }) => {
+  const [isFlipped, setIsFlipped] = React.useState(false);
+  const classes = useStyles();
+
+  return (
+    <Grid
+      container
+      className={classes.tileInnerContainer}
+      justify="center"
+      item
+      sm={4}
+      xs={6}
+    >
+      <ReactCardFlip
+        flipSpeedBackToFront={0.9}
+        flipSpeedFrontToBack={0.9}
+        isFlipped={isFlipped}
+        infinite={true}
+        flipDirection={"vertical"}
+      >
+        <Grid
+          container
+          className={classes.tile}
+          onClick={() => setIsFlipped(!isFlipped)}
+          justify="center"
+          alignItems="center"
+        >
+          {front}
+        </Grid>
+        <Grid
+          container
+          className={clsx([classes.tile, classes.tileBack])}
+          onClick={() => setIsFlipped(!isFlipped)}
+          justify="center"
+          alignItems="center"
+        >
+          {back}
+        </Grid>
+      </ReactCardFlip>
+    </Grid>
+  );
+};
--- src/site-pages/About/Program-Overview/styles.jsx
+++ src/site-pages/About/Program-Overview/styles.jsx
@@ -1,15 +1,85 @@
 import { makeStyles } from "@material-ui/core";
 
+import background from "../../../assets/background.png";
+
 export const useStyles = makeStyles((theme) => ({
   container: {
     display: "flex",
-    backgroundColor: "#02274C",
-    paddingTop: "2rem",
+    minHeight: "90vh",
   },
   innerContainer: {
-    [theme.breakpoints.down("sm")]: {
-      paddingTop: "1rem",
+    height: "fit-content",
+  },
+  heroContainer: {
+    background: `url("${background}")`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    height: "90vh",
+    overflow: "hidden",
+    paddingTop: "2rem",
+    [theme.breakpoints.up("sm")]: {
+      height: "100vh",
     },
-    visibility: "hidden",
+  },
+  paragraphsContainer: {
+    padding: "5rem",
+    backgroundColor: "#EAEAEA",
+    [theme.breakpoints.down("xs")]: {
+      padding: "2rem",
+    },
+  },
+  smallText: {
+    fontSize: "1.2rem",
+    lineHeight: "2.5rem",
+    textAlign: "justify",
+    padding: "2rem 0",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1.1rem",
+    },
+  },
+  largeText: {
+    fontSize: "1.875rem",
+    lineHeight: "4rem",
+    textAlign: "center",
+    color: "#00274C",
+    textTransform: "capitalize",
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1.5rem",
+    },
+  },
+  statOverview: {
+    padding: "7rem 0",
+    backgroundColor: "#FFFAFA",
+  },
+  tileOuterContainer: {
+    paddingTop: "4rem",
+  },
+  tileContainer: {
+    backgroundColor: "#00274C",
+    borderRadius: "1.875rem",
+    padding: "1rem",
+  },
+  tile: {
+    width: "11rem",
+    height: "11rem",
+    backgroundColor: "#EAEAEA",
+    borderRadius: "1.875rem",
+    [theme.breakpoints.down("xs")]: {
+      width: "9rem",
+      height: "9rem",
+      fontSize: "1rem",
+    },
+    padding: "1rem",
+    textAlign: "center",
+    fontFamily: "Merriweather",
+    fontSize: "1.2rem",
+    fontWeight: "bold",
+    color: "#00274C",
+  },
+  tileInnerContainer: {
+    padding: "1rem",
+  },
+  tileBack: {
+    fontSize: "1.5rem",
   },
 }));
