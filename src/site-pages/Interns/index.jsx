a265ed93f59f3968fbf048da015c72be49086ccf
nate2427 <nlbaker@umich.edu>
2020-10-15 15:15:54 -0500

    interns is all cleaned up!

--- src/site-pages/Interns/index.jsx
+++ src/site-pages/Interns/index.jsx
@@ -7,7 +7,7 @@ import { get_content } from "../../shared/Http";
 
 import { useStyles } from "./styles";
 
-export default function History() {
+export default function Interns() {
   const classes = useStyles();
   const [content, setContent] = React.useState(null);
 
@@ -40,11 +40,30 @@ export default function History() {
         </Grid>
         {/* main container */}
         <Grid container justify="center" className={classes.mainContainer}>
-          <Grid container item sm={10}>
-            {/* <PreceptorYear
-              year={content && content.years[0].year}
-              imgSrc={pics[0]}
-            /> */}
+          <Grid
+            container
+            item
+            xs={11}
+            xl={9}
+            className={classes.descriptionContainer}
+          >
+            <Typography variant="body1" className={classes.description}>
+              {content && content.description}
+            </Typography>
+          </Grid>
+          <Grid container item xs={11} xl={9}>
+            {content &&
+              content.years
+                .reverse()
+                .map((year, key) => (
+                  <InternYear
+                    key={key}
+                    year={content && year.year}
+                    where={content && content.wheres[key]}
+                    interns={content && year.interns}
+                    schools={content && year.undergrad_institutions}
+                  />
+                ))}
           </Grid>
         </Grid>
       </Grid>
@@ -52,19 +71,153 @@ export default function History() {
   );
 }
 
-const PreceptorYear = ({ year, imgSrc, title }) => {
+const InternYear = ({ year, where, interns, schools }) => {
   const classes = useStyles();
 
   return (
-    <Grid container className={classes.yearContainer}>
-      <Typography variant="h2">{year}</Typography>
-      <Grid container item md={4} sm={3}>
-        <img src={imgSrc} alt={"preceptor hightight"} />
-      </Grid>
-      <Grid container item md={6} sm={8}>
-        <Paper className={classes.yearCard} elevation={10}>
-          Hello
-        </Paper>
+    <Grid container className={classes.yearContainer} justify="center">
+      <Typography
+        style={{
+          fontSize: "3rem",
+          fontFamily: "Merriweather, Merriweather sans",
+          color: "#00274C",
+        }}
+        className={classes.year}
+        variant="h2"
+      >
+        {year}
+      </Typography>
+      <Grid
+        container
+        style={{ padding: "5rem 0" }}
+        justify="center"
+        item
+        xs={11}
+      >
+        <Grid className={classes.paperContainer} container item sm={4}>
+          <Paper className={classes.yearCard} elevation={10}>
+            <Typography
+              style={{
+                color: "#FFCB3D",
+                fontFamily: "Merriweather, Merriweather sans",
+                fontSize: "1rem",
+                paddingBottom: "1rem",
+                fontWeight: "bold",
+              }}
+              variant="h5"
+            >
+              Interns
+            </Typography>
+            {interns.map((intern, key) => (
+              <Typography key={key} className={classes.intern} variant="body1">
+                {intern.name}
+              </Typography>
+            ))}
+          </Paper>
+        </Grid>
+        <Grid className={classes.paperContainer} container item sm={4}>
+          <Paper className={classes.yearCard} elevation={10}>
+            <Typography
+              style={{
+                color: "#FFCB3D",
+                fontFamily: "Merriweather, Merriweather sans",
+                fontSize: "1rem",
+                paddingBottom: "1rem",
+                fontWeight: "bold",
+                align: "center",
+                width: "100%",
+              }}
+              variant="h5"
+            >
+              Undergraduate Institutions
+            </Typography>
+            {schools.map((school, key) => (
+              <Typography key={key} className={classes.intern} variant="body1">
+                {school.school}
+              </Typography>
+            ))}
+          </Paper>
+        </Grid>
+        <Grid className={classes.paperContainer} container item sm={4}>
+          <Paper className={classes.yearCard} elevation={10}>
+            <Typography
+              style={{
+                color: "#FFCB3D",
+                fontFamily: "Merriweather, Merriweather sans",
+                fontSize: "1rem",
+                paddingBottom: "1rem",
+                fontWeight: "bold",
+              }}
+              variant="h5"
+            >
+              Where They Are Now
+            </Typography>
+            {where.student_healths.length > 0 && (
+              <Grid container className={classes.industryContainer}>
+                <Typography className={classes.industryTitle} variant="body1">
+                  Health Industry Occupations:
+                </Typography>
+                {where.student_healths.map((industry, key) => (
+                  <Typography
+                    key={key}
+                    className={classes.industry}
+                    variant="body1"
+                  >
+                    {industry.industry}
+                  </Typography>
+                ))}
+              </Grid>
+            )}
+            {where.other_industries.length > 0 && (
+              <Grid container className={classes.industryContainer}>
+                <Typography className={classes.industryTitle} variant="body1">
+                  Other Industries:
+                </Typography>
+                {where.other_industries.map((industry, key) => (
+                  <Typography
+                    key={key}
+                    className={classes.industry}
+                    variant="body1"
+                  >
+                    {industry.industry}
+                  </Typography>
+                ))}
+              </Grid>
+            )}
+            {where.public_healths.length > 0 && (
+              <Grid container className={classes.industryContainer}>
+                <Typography className={classes.industryTitle} variant="body1">
+                  Public Health Graduate Education:
+                </Typography>
+                {where.public_healths.map((industry, key) => (
+                  <Typography
+                    key={key}
+                    className={classes.industry}
+                    variant="body1"
+                  >
+                    {industry.school}
+                  </Typography>
+                ))}
+              </Grid>
+            )}
+            {where.other_grads.length > 0 && (
+              <Grid container className={classes.industryContainer}>
+                <Typography className={classes.industryTitle} variant="body1">
+                  Other Graduate Education:
+                </Typography>
+                {where.other_grads.map((industry, key) => (
+                  <Typography
+                    key={key}
+                    className={classes.industry}
+                    variant="body1"
+                  >
+                    {industry.gradschool}
+                  </Typography>
+                ))}
+              </Grid>
+            )}
+          </Paper>
+        </Grid>
       </Grid>
     </Grid>
   );
--- src/site-pages/Interns/styles.jsx
+++ src/site-pages/Interns/styles.jsx
@@ -12,6 +12,9 @@ export const useStyles = makeStyles((theme) => ({
   heroOuterContainer: {
     height: "100vh",
     overflow: "hidden",
+    [theme.breakpoints.down("sm")]: {
+      height: "auto",
+    },
   },
   heroContainer: {
     paddingTop: "2rem",
@@ -27,7 +30,7 @@ export const useStyles = makeStyles((theme) => ({
     },
     [theme.breakpoints.down("xs")]: {
       // height: "100vh",
-      backgroundSize: "140%",
+      height: "43vh",
     },
     [`${theme.breakpoints.down("sm")} and (orientation: landscape)`]: {
       height: "120vh",
@@ -35,5 +38,59 @@ export const useStyles = makeStyles((theme) => ({
   },
   yearCard: {
     width: "100%",
+    backgroundColor: "#00274C",
+    padding: "1rem",
+  },
+  year: {
+    width: "100%",
+  },
+  mainContainer: {
+    padding: "2rem 0",
+  },
+  descriptionContainer: {
+    paddingBottom: "4rem",
+
+    [theme.breakpoints.up("xl")]: {
+      paddingBottom: "6rem",
+    },
+  },
+  description: {
+    fontSize: "1rem",
+    textAlign: "justify",
+    lineHeight: "2rem",
+    [theme.breakpoints.up("xl")]: {
+      fontSize: "1.5rem",
+      lineHeight: "3rem",
+    },
+  },
+  paperContainer: {
+    padding: "0 1rem",
+    [theme.breakpoints.down("xs")]: {
+      padding: "2rem 0",
+    },
+  },
+  intern: {
+    color: "#fff",
+    opacity: ".6",
+    fontFamily: "Merriweather sans, sans-serif",
+    width: "100%",
+    textAlign: "center",
+    padding: ".5rem 0",
+  },
+  industryTitle: {
+    color: "#fff",
+    fontFamily: "Merriweather, Merriweather sans",
+    fontWeight: "bold",
+  },
+  industry: {
+    color: "#fff",
+    opacity: ".6",
+    fontFamily: "Merriweather sans, sans-serif",
+    width: "100%",
+    textAlign: "center",
+    padding: ".5rem 0",
+  },
+  industryContainer: {
+    paddingBottom: "1rem",
   },
 }));
